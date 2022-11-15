7a61902fad96263929d6ae4ba3b54b523f02c807
Vladimir Bazilevich <v.bazilevich@innopolis.university>
2022-11-15 09:09:17 +0300

    Prepared ColorFool interface for API

---
+++ ColorFool.py
@@ -0,0 +1,41 @@
+import os
+import cv2
+import numpy as np
+
+NUM_ITER = 3
+
+def segment():
+    # Move to segmentation
+    os.chdir('Segmentation')
+
+    # Run segmentation
+    os.system('./script.sh')
+
+    # Return to repository root
+    os.chdir('..')
+
+def adversarial():
+    # Move to adversarial
+    os.chdir('Adversarial')
+
+    # Run adversarial Attack
+    os.system('./script.sh')
+
+    # Return to the root
+    os.chdir('..')
+
+def colorize(img_bin: bytes) -> bytes:
+    image = cv2.imdecode(np.frombuffer(img_bin, np.uint8), cv2.IMREAD_COLOR)
+    cv2.imwrite('Dataset/image.png', image)
+
+    segment()
+    adversarial()
+
+    with open('Adversarial/Results/ColorFoolImgs/adv_resnet50/image.png', 'rb') as f:
+        out = f.read()
+        return out
+
+if __name__ == "__main__":
+    with open('Dataset/image.png', 'rb') as fp:
+        im_b = fp.read()
+        colorize(im_b)
